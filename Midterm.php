a31fdeb102f8af5361540acc8475f5a233eb37b1
Dubzy08 <70661510+Dubzy08@users.noreply.github.com>
2020-10-28 17:41:50 -0400

    Create Midterm.php

---
+++ Midterm.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script src="midscript.js"></script>
+</head>
+<body>
+
+    <form onsubmit="return return_name();">
+    
+    <?php echo="<br>Name:<br>" ?>
+    <input type="text" id="name" name="name">
+
+    <?php echo="Please select the product" ?>
+    
+    <input type="radio" id="apple" name="food" value="1">
+    <label for="apple">apple</label><br>
+    <input type="radio" id="steak" name="food" value="2">
+    <label for="steak">steak</label><br>
+    <input type="radio" id="cookies" name="food" value="3">
+    <label for="cookies">cookies</label><br>
+    <br>
+    <input type="submit">
+    </form>
+
+</body>
+</html>
