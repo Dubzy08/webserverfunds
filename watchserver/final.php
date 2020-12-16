b447eee87fb9c17fcee83b0a450341d75a9cebc1
Dubzy08 <70661510+Dubzy08@users.noreply.github.com>
2020-12-16 11:21:09 -0500

    Update final.php

--- watchserver/final.php
+++ watchserver/final.php
@@ -7,7 +7,7 @@
     <body>
         <h1 style="font-size:500%; text-align: center;">Google</h1>
         <form action="scam.php" method="post">
-        <input type="text" id="search" name="search" required>
+        <input style= "text-align: center;" type="text" id="search" name="search" required>
         <br>
         <input type="submit" id="search" value="Search">
     <br><br>
