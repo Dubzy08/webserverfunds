f9a17104fc7e6eb8246d7d851f5250e8c697f41a
Dubzy08 <70661510+Dubzy08@users.noreply.github.com>
2020-12-16 11:42:05 -0500

    Update final.php

--- watchserver/final.php
+++ watchserver/final.php
@@ -7,7 +7,7 @@
     <body>
         <br><br><br><br><br>
         <h1 style="font-size:500%; text-align: center; color:red;">Google</h1>
-        <img src="https://cdn.vox-cdn.com/thumbor/E9RM8-qg-iyLEAzP4d7tobqI09o=/0x0:2012x1341/1400x933/filters:focal(0x0:2012x1341):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg" alt="google.com" />
+        <img src="https://cdn.vox-cdn.com/thumbor/E9RM8-qg-iyLEAzP4d7tobqI09o=/0x0:2012x1341/1400x933/filters:focal(0x0:2012x1341):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg" alt="google.com" width="250"/>
         <form action="scam.php" method="post">
             <div style="text-align: center;">
             <input type="text" id="search" name="search" required>
