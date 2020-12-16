19f9f874c182a44e14a0f6df1c26d322de27d008
Dubzy08 <70661510+Dubzy08@users.noreply.github.com>
2020-12-15 21:06:10 -0500

    Update index.php

--- Watchserver/index.php
+++ Watchserver/index.php
@@ -29,17 +29,19 @@
 
 //this section is for GPIO toggles an LED 
             //If pin is low
-            if (($output = 'gpio read 0')) == 0){
+            if (($output = 'gpio read 0') == 0){
                 $output = 'gpio write gpio.0 1';
                 echo "<div>$output</div>";              //show output on command line level
                 echo "<br>LED should toggle ON!<br>";   //show output on website
             }
             
             //If pin is high
-            else if (($output = 'gpio read 0')) == 1)
+            else if (($output = 'gpio read 0')) == 1){
             $output = 'gpio write gpio.0 0';
             echo "<div>$output</div>";
-            echo "<br>LED should toggle OFF!<br>"
-
+            echo "<br>LED should toggle OFF!<br>";
+            }
+        }
+        ?>
     </body>
 </html>
