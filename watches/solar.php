f2badd9ad0a0240a3f8a5d30e082cc50a9e6751f
Dubzy08 <70661510+Dubzy08@users.noreply.github.com>
2020-12-20 16:01:40 -0500

    Create solar.php

---
+++ watches/solar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <h2>
+            Kinetc Watches
+        </h2>
+    </head>
+    <body>
+        <div>
+            Kinetc watches have a very particular way of working. This type of watch is<br>
+            basically the combination of an automatic watch and a quartz watch. This is<br>
+            <br><br>
+            Here we can see the inside of a luxury mecahnical watch:<br>
+            <img src="https://i.ytimg.com/vi/4zQdxie31kI/maxresdefault.jpg" alt="Solar watch" width="400" />
+            <br>
+            <br>
+            <a href="index.php">Return</a> to main page
+        </div>
+    </body>
+</html>
