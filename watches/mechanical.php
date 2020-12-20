56cb21044e3a995bbb4430ae79b085839e9e455a
Dubzy08 <70661510+Dubzy08@users.noreply.github.com>
2020-12-20 13:16:57 -0500

    Update mechanical.php

--- watches/mechanical.php
+++ watches/mechanical.php
@@ -7,7 +7,13 @@
     </head>
     <body>
         <div>
-            These are all mechanical watches with different complications
+            A mechanical watch is a watch that has no batteries or mini circuits inside.<br>
+            The way it works is that a spring called the Main Spring holds energy by being<br>
+            sprung in the oppsite direction. This spring desprings as the time goes by.<br>
+            To recharge the watch, you must manually turn the crown which will respring the watch.
+            <br>
+            <img src="https://www.matthewbaininc.com/assets/images/watches/Patek-3970EG/patek-3970eg-z2.jpg" alt="Mechanical watch" width="250" />
+            <br>
             <br>
             <a href="index.php">Return</a> to main page
         </div>
