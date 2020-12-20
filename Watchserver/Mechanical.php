17a1f8fd9ffa284b0be15fefebc454df673fffb9
Dubzy08 <70661510+Dubzy08@users.noreply.github.com>
2020-12-20 12:33:23 -0500

    Update Mechanical.php

--- Watchserver/Mechanical.php
+++ Watchserver/Mechanical.php
@@ -8,6 +8,10 @@
     <body>
         <div>
             These are all mechanical watches with different complications
+            <br>
+            <from action="index.php">
+                <input type="submit" value="Return">
+            </form>
         </div>
     </body>
 </html>
