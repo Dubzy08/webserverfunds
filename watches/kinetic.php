658ad1bbbf439d26377c3261af49a99895720ddd
Dubzy08 <70661510+Dubzy08@users.noreply.github.com>
2020-12-20 15:48:17 -0500

    Create kinetic.php

---
+++ watches/kinetic.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <h2>
+            Kinetc Watches
+        </h2>
+    </head>
+    <body>
+        <div>
+            Kinetc watches have a very particular way of working. This type of watch is<br>
+            basically the combination of an automatic watch and a quartz watch. This is<br>
+            because the majority of the watch is built with mechanical equipment which<br>
+            is used to charge it. However, instead of charging a spring, the watch charges<br>
+            a capacitor which allows a mini circuit to work in parallel in the watch. This<br>
+            functionalit also gives the watch a unique feature. It is able to stop running<br>
+            when the user is not wearing it anymore but keeps the time nonetheless and<br>
+            returns to the actual time when the user puts it back on.
+            <br><br>
+            Here we can see the inside of a luxury mecahnical watch:<br>
+            <img src="https://media.gq-magazine.co.uk/photos/5d139ee0566301252d975906/16:9/w_1920,c_limit/Seiko-01-GQ-18May17_b.jpg" alt="Kinetic watch" width="400" />
+            <br>
+            <br>
+            <a href="index.php">Return</a> to main page
+        </div>
+    </body>
+</html>
